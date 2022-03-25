34769379a2c42bafc4f903658d949582f0424316
Ashutosh Thorat <ashutoshrit64@gmail.com>
2022-03-25 08:24:05 +0530

    Added employee attendence code

--- src/com/bridgelabz/HelloWorld.java
+++ src/com/bridgelabz/HelloWorld.java
@@ -1,9 +1,18 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("please enter the value between 0 and 1");
+		int userInput=scanner.nextInt();
+		if(userInput==0) {
+			System.out.println("employee is present");
+		}else {
+			System.out.println("employee is absent");
+		}
 
 	}
 
